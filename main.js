76b98c1c24a9685e6fc2f4579f247f5bdded5a62
dingdongbyju <103965231+dingdongbyju@users.noreply.github.com>
2023-03-21 15:43:48 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+img = "";
+
+function preload() {
+    img = loadImage("hqdefault.jpg");
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+    fill("#ffd700")
+}
